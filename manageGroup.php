76204fb5985daa8854f08fae2dd0c598a4ba290e
Brandon Chong <phototix@hotmail.com>
2025-06-13 15:52:12 +0800

    Update

--- manageGroup.php
+++ manageGroup.php
@@ -81,7 +81,9 @@ $groupPic = $picData['url'] ?? 'https://cloud.webbypage.com/index.php/s/kwzFAtin
                         data-grouppic="<?= htmlspecialchars($groupPic) ?>">
                     <i class="bi bi-pencil"></i> Edit
                 </button>
-                
+                <a href="/">
+                    <button class="btn btn-sm btn-outline-primary">Tasks</button>
+                </a>
                 <div class="d-flex align-items-center mb-3">
                     <img src="<?= htmlspecialchars($groupPic) ?>" class="group-avatar" alt="Group Picture">
                     <div>
