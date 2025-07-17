3fdea7730f5e9b802175eac372deada708897978
Brandon Chong <phototix@hotmail.com>
2025-07-17 14:08:00 +0800

    Reorder Task list

--- api/tasks.php
+++ api/tasks.php
@@ -16,7 +16,7 @@ switch ($method) {
     case 'GET':
         // Get all tasks for the user for today
         if ($userId) {
-            $query = "SELECT * FROM daily_tasks WHERE user_id = :user_id ORDER BY priority DESC, task_date DESC";
+            $query = "SELECT * FROM daily_tasks WHERE user_id = :user_id ORDER BY priority DESC, task_date ASC";
             $stmt = $db->prepare($query);
             $stmt->bindParam(':user_id', $userId);
             $stmt->execute();
