19b898aa9eaabea14a4ef7eb581410a51ab851e0
Brandon Chong <phototix@hotmail.com>
2025-07-17 10:37:13 +0800

    Update

--- ticket.php
+++ ticket.php
@@ -4,16 +4,13 @@
     <meta charset="UTF-8">
     <title>Tickets</title>
     <link rel="icon" type="image/x-icon" href="/favicon.ico">
-
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-
     <!-- Bootstrap Bundle JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-
     <!-- jQuery -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
