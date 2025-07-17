e8b6c13b30f049ac4dea179126564ec3e33a20d3
Brandon Chong <phototix@hotmail.com>
2025-07-17 10:19:55 +0800

    Update

--- api/contactUpdate.php
+++ api/contactUpdate.php
@@ -2,7 +2,7 @@
 require_once '../config/database.php';
 
 try {
-  $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8mb4", $user, $pass);
+  $pdo = new PDO("mysql:host=$db_host;dbname=$db_name;charset=utf8mb4", $db_user, $db_pass);
   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
   die(json_encode(["error" => $e->getMessage()]));
